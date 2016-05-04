3d5911d7a7d0004b425bfe4c930ca510ce683ed9
Tom <tom.odell@strident.co.uk>
2016-05-04 21:14:21 +0100

    Added new exercise types

--- TrainingLog/Initialiser.cs
+++ TrainingLog/Initialiser.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingLog.Models;
 
 namespace TrainingLog
 {
@@ -10,7 +12,13 @@ namespace TrainingLog
     {
         protected override void Seed(Context context)
         {
-            // populate exercise types here
+            context.ExerciseTypes.AddOrUpdate(m => m.Id,
+                new ExerciseType { Name = "Squat" },
+                new ExerciseType { Name = "Press" },
+                new ExerciseType { Name = "Chin-up" },
+                new ExerciseType { Name = "Front squat" },
+                new ExerciseType { Name = "Inverted row" }
+                );
 
             context.SaveChanges();
         }
