f4f055c6bbf38ce6ad4bdbb952e091dddc989e11
Tom <tom.odell@strident.co.uk>
2016-06-20 18:48:42 +0100

    Start of list for log books

---
+++ TrainingLog/Models/LogBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingLog.Models
+{
+    public class LogBook : BaseModel
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ICollection<Log> Logs { get; set; }
+    }
+}
---
+++ TrainingLog/ViewModels/LogBooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingLog.Models;
+
+namespace TrainingLog.ViewModels
+{
+    public class LogBooksViewModel
+    {
+        public ICollection<LogBook> LogBooks { get; set; }
+    }
+}
