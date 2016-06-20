3c2699c07c74464796dcafdf60c24b37983c144a
Tom <tom.odell@strident.co.uk>
2016-06-20 18:52:02 +0100

    logbooks controller and create link

---
+++ TrainingLog/Controllers/LogBooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrainingLog.ViewModels;
+
+namespace TrainingLog.Controllers
+{
+    public class LogBooksController : Controller
+    {
+        // GET: LogBooks
+        public ActionResult Index()
+        {
+            var vm = new LogBooksViewModel();
+            return View(vm);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new LogBooksViewModel());
+        }
+    }
+}
